44fd35428c4c8fb599bbc230e5c61815b70b9c18
Hari <haricarbajal3@gmail.com>
2024-12-04 00:03:12 +0100

    Interfaz grafica agregada

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -40,34 +40,74 @@ public class Main {
                     String region = location.getString("region");
                     System.out.println("Esta es la region: " + region);
 
+
+                    //icono
                     JSONObject current = json.getJSONObject("current");
                     JSONObject condition = current.getJSONObject("condition");
                     String icon = condition.getString("icon");
 
 
+                    //temp_f
+                    String tempf = String.valueOf(current.getInt("temp_f"));
+
+
+
+                    //time
+                    String time = location.getString("localtime");
                     SwingUtilities.invokeLater(() -> {
                         // Crear el marco (ventana)
                         JFrame frame = new JFrame("Cimapp");
-                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Cierra la app al salir
+                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Cierra la app al salir
 
+                        // Usar null layout para manejar las posiciones manualmente
+                        JPanel panel = new JPanel();
+                        panel.setLayout(null); // Desactivar LayoutManager
 
-                        ImageIcon imagen = downloadImage( "https:"+ icon);
-
-
-                        // Crear una etiqueta con el icono de la api
-                        JLabel labelImage = new JLabel(imagen, SwingConstants.CENTER);
-                        labelImage.setVerticalAlignment(SwingConstants.TOP);
-                        // Agregar la etiqueta al marco
-                        frame.add(labelImage);
-
+                        //Region
                         JLabel regionLabel = new JLabel(region);
-                        regionLabel.setBounds(50,50, 640, 640);
-                        frame.add(labelImage);
+                        regionLabel.setVerticalAlignment(SwingConstants.TOP);
+                        regionLabel.setHorizontalAlignment(SwingConstants.CENTER);
+                        regionLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+                        regionLabel.setBounds(100, 200, 400, 50);
+                        panel.add(regionLabel);
 
-                        frame.add(regionLabel, BorderLayout.NORTH);
+
+                        // Imagen
+                        ImageIcon imagen = downloadImage("https:" + icon);
+                        Image imagenEscalada = imagen.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+                        ImageIcon nuevaImg = new ImageIcon(imagenEscalada);
+                        JLabel labelImage = new JLabel(nuevaImg);
+                        labelImage.setVerticalAlignment(SwingConstants.CENTER);
+                        labelImage.setBounds(205, 250, 200, 100);
+                        panel.add(labelImage);
+
+
+                        //temperatura
+                        JLabel tempfLabel = new JLabel(tempf + "ºF");
+                        tempfLabel.setHorizontalAlignment(SwingConstants.CENTER);
+                        tempfLabel.setVerticalAlignment(SwingConstants.CENTER);
+                        tempfLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+                        tempfLabel.setBounds(150, 330, 300, 50);
+                        panel.add(tempfLabel);
+
+
+                        // Time
+                        JLabel timeLabel = new JLabel(time);
+                        timeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+                        timeLabel.setVerticalAlignment(SwingConstants.BOTTOM);
+                        timeLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+                        timeLabel.setBounds(150, 350, 300, 50);
+                        panel.add(timeLabel);
+
+
+                        // Agregar el panel al frame
+                        frame.add(panel);
+
 
                         // Ajustar el tamaño de la ventana para que se acomode al contenido
                         frame.setSize(600, 800);  // Ancho x Alto
+
+
                         // Mostrar la ventana
                         frame.setVisible(true);
                     });
@@ -105,6 +145,7 @@ public class Main {
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
+
             throw new RuntimeException(e);
         }
     }
